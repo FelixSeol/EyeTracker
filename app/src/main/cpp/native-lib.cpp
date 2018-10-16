f08b441927eb28468a7ee112286c2249c5d2cd05
FelixSeol <felixxseol@gmail.com>
2018-10-17 00:09:00 +0900

    trishume eyecenter added.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,9 @@
 using namespace cv;
 using namespace std;
 
+cv::Point lastPoint;
+std::vector<cv::Point> centers;
+cv::Point mousePoint;
 
 float resize(Mat img_src, Mat &img_resize, int resize_width){
     float scale = resize_width / (float)img_src.cols ;
@@ -39,9 +42,24 @@ Point stabilize(std::vector<cv::Point> &points, int windowSize)
     return Point(sumX, sumY);
 }
 
-cv::Vec3f getEyeball(cv::Mat &eye, std::vector<cv::Vec3f> &circles)
+cv::Rect getLeftmostEye(std::vector<cv::Rect> &eyes)
 {
-    std::vector<int> sums(circles.size(), 0);
+    int leftmost = 99999999;
+    int leftmostIndex = -1;
+    for (int i = 0; i < eyes.size(); i++)
+    {
+        if (eyes[i].tl().x < leftmost)
+        {
+            leftmost = eyes[i].tl().x;
+            leftmostIndex = i;
+        }
+    }
+    return eyes[leftmostIndex];
+}
+
+Vec3f getEyeball(Mat &eye, vector<Vec3f> &circles)
+{
+    vector<int> sums(circles.size(), 0);
     for (int y = 0; y < eye.rows; y++)
     {
         uchar *ptr = eye.ptr<uchar>(y);
@@ -50,9 +68,9 @@ cv::Vec3f getEyeball(cv::Mat &eye, std::vector<cv::Vec3f> &circles)
             int value = static_cast<int>(*ptr);
             for (int i = 0; i < circles.size(); i++)
             {
-                cv::Point center((int)std::round(circles[i][0]), (int)std::round(circles[i][1]));
-                int radius = (int)std::round(circles[i][2]);
-                if (std::pow(x - center.x, 2) + std::pow(y - center.y, 2) < std::pow(radius, 2))
+                Point center((int)round(circles[i][0]), (int)round(circles[i][1]));
+                int radius = (int)round(circles[i][2]);
+                if (pow(x - center.x, 2) + pow(y - center.y, 2) < pow(radius, 2))
                 {
                     sums[i] += value;
                 }
@@ -101,9 +119,7 @@ Java_com_example_felixxseol_eyetracker_MainActivity_detect(JNIEnv *env, jobject 
     Mat &img_input = *(Mat *) matAddrInput;
     Mat &img_result = *(Mat *) matAddrResult;
     img_result = img_input.clone();
-//    img_result = Mat(img_input.size(), img_input.type(), Scalar::all(2));
 
-    std::vector<Rect> faces;
     Mat img_gray;
 
     cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
@@ -112,56 +128,41 @@ Java_com_example_felixxseol_eyetracker_MainActivity_detect(JNIEnv *env, jobject 
     float resizeRatio = resize(img_gray, img_resize, 640);
 
     //-- Detect faces
-    ((CascadeClassifier *) cascadeClassifier_face)->detectMultiScale( img_resize, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(200, 200) );
+    std::vector<Rect> faces;
+    ((CascadeClassifier *) cascadeClassifier_face)->detectMultiScale( img_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(400, 400) );
     if(faces.size() == 0) return;
     __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "face %d found. ", faces.size());
-    for( int i = 0; i < faces.size(); i++) {
-        double real_facesize_x = faces[0].x / resizeRatio;
-        double real_facesize_y = faces[0].y / resizeRatio;
-        double real_facesize_width = faces[0].width / resizeRatio;
-        double real_facesize_height = faces[0].height / resizeRatio;
-        Scalar rect_color;
-        Point center(real_facesize_x + real_facesize_width / 2,
-                     real_facesize_y + real_facesize_height / 2);
-
-        Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width, real_facesize_height);
-        __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "Face ROI X : %f, Y : %f", real_facesize_x, real_facesize_y);
-        __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "Face ROI width : %f, height : %f", real_facesize_width, real_facesize_height);
-
-        rect_color = Scalar(0, 255, 255);
-        img_input(face_area).copyTo(img_result(face_area));
-
-        Mat faceROI = img_gray(face_area);
-        std::vector<Rect> eyes;
-        //-- In each face, detect eyes
-        ((CascadeClassifier *) cascadeClassifier_eye)->detectMultiScale(faceROI, eyes, 1.1, 2,
-                                                                        0 | CASCADE_SCALE_IMAGE,
-                                                                        Size(80, 80));
-        if(eyes.size() != 2) return;
-
-        for (size_t j = 0; j < eyes.size(); j++) {
-            double real_eye_x = real_facesize_x + eyes[j].x;
-            double real_eye_y = real_facesize_y + eyes[j].y;
-            double real_eye_width = eyes[j].width;
-            double real_eye_height = eyes[j].height;
-            Point eye_center(real_eye_x + real_eye_width / 2,
-                             real_eye_y + real_eye_height/ 2);
-            __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "Eye ROI X : %f, Y : %f", real_eye_x, real_eye_y);
-            __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "Eye ROI width : %f, height : %f", real_eye_width, real_eye_width);
-            Rect eye_area(real_eye_x, real_eye_y, eyes[j].width, eyes[j].height);
-            if (real_eye_y < 500) {
-                rectangle(img_result, eye_area, Scalar(255, 0, 0), 5, 8, 0);
-            }
-//              int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-//              circle(img_result, eye_center, radius, Scalar(255, 0, 0), 5, 8, 0);
-            Mat eyeROI = img_gray(eye_area);
-            equalizeHist(eyeROI, eyeROI);
-
-            std::vector<Vec3f> circles;
-            HoughCircles(eyeROI, circles, CV_HOUGH_GRADIENT, 1, eyeROI.cols / 8, 250, 15, eyeROI.rows / 8, eyeROI.rows / 3);
-            cv::circle(eyeROI, center, radius, cv::Scalar(255, 255, 255), 2);
+    Mat faceROI = img_gray(faces[0]);
+    std::vector<Rect> eyes;
+    //-- In each face, detect eyes
+    ((CascadeClassifier *) cascadeClassifier_eye)->detectMultiScale(faceROI, eyes, 1.1, 2,
+                                                                    0 | CASCADE_SCALE_IMAGE,
+                                                                    Size(80, 80));
+    rectangle(img_result, faces[0].tl(), faces[0].br(), Scalar(0, 255, 255), 2);
+    if(eyes.size() != 2) return;
+    for (Rect &eye : eyes) {
+        rectangle(img_result, faces[0].tl() + eye.tl(), faces[0].tl() + eye.br(), Scalar(255, 255, 255), 2);
+        __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ", (char *) "eyes %d found. ", eyes.size());
+    }
+    Rect eyeRect = getLeftmostEye(eyes);
+    Mat eyeROI = img_gray(eyeRect);
+    equalizeHist(eyeROI, eyeROI);
+    std::vector<Vec3f> circles;
+    HoughCircles(eyeROI, circles, CV_HOUGH_GRADIENT, 1, eyeROI.cols / 8, 250, 15, eyeROI.rows / 8, eyeROI.rows / 3);
+    if (circles.size() > 0){
+        Vec3f eyeball = getEyeball(eyeROI, circles);
+        Point center(eyeball[0], eyeball[1]);
+        centers.push_back(center);
+        center = stabilize(centers, 3);
+        if (centers.size() > 1)
+        {
+            Point diff;
+            diff.x = (center.x - lastPoint.x) * 20;
+            diff.y = (center.y - lastPoint.y) * -30;
         }
-        rectangle(img_result, face_area, rect_color, 8, 8, 0);
-
+        lastPoint = center;
+        int radius = (int)eyeball[2];
+        circle(img_result, faces[0].tl() + eyeRect.tl() + center, radius, Scalar(0, 0, 255), 2);
+        circle(eyeROI, center, radius, cv::Scalar(255, 255, 255), 2);
     }
 }
